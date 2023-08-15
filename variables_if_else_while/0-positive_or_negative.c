61af6a4eba49930d053ad672282aaf7ce63308e7
Mayra Vasquez <mayravas2808@gmail.com>
2023-08-14 19:37:09 -0700

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,13 +5,11 @@
 /* betty style doc for function main goes there */
 /**
  * main - main function
- *
  * Return: always 0
  */
 int main(void)
 {
 		int n;
-		
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
 				/* your code goes there */
